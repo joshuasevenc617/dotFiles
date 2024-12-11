4b575eb05728d1f85c53887296d76b1f04b9d5db
joshuasevenc617 <joshuasevenc@gmail.com>
2024-12-12 00:43:16 +1100

    update vim tmux navigator for nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,10 +1,20 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+unmap("n", "<c-h>")
+unmap("n", "<c-j>")
+unmap("n", "<c-k>")
+unmap("n", "<c-l>")
+map("n", "<c-h>", "<cmd>:TmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>:TmuxNavigateRight<cr>")
+map("n", "<c-\\>", "<cmd>:TmuxNavigatePrevious<cr>")
