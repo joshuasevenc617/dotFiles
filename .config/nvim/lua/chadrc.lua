2002c73ffdefe85738e4a73aeb383729218aca37
joshuasevenc617 <joshuasevenc@gmail.com>
2025-01-26 16:15:27 +1100

    remove unused config

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -9,10 +9,6 @@ M.base46 = {
 	theme = "catppuccin",
   transparency = true,
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
   hl_override = {
     ["@comment"] = { italic = true },
     Type = { italic = true },
