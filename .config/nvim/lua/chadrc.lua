477f5131269c28f5fabb43c000af72163a9caac9
joshuasevenc617 <joshuasevenc@gmail.com>
2025-01-26 16:14:36 +1100

    add italic fonts support

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -13,6 +13,29 @@ M.base46 = {
 		Comment = { italic = true },
 		["@comment"] = { italic = true },
 	},
+  hl_override = {
+    ["@comment"] = { italic = true },
+    Type = { italic = true },
+    AlphaHeader = { fg = "blue" },
+    CursorLine = {
+      bg = "black2",
+    },
+    Comment = {
+      italic = true,
+    },
+    Define = { italic = true },
+    eliixirString = { italic = true },
+    Include = { italic = true },
+    TSVariable = { italic = true },
+    Variable = { italic = true },
+    Function = { italic = true },
+    Keyword = { italic = true },
+    TSKeyword = { italic = true },
+    TSMethod = { italic = true },
+    TSDefine = { italic = true },
+    SpecialComment = { italic = true },
+    Statement = { italic = true },
+  },
 }
 
 M.nvdash = {
